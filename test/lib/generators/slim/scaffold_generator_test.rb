6aaff8a18fe94dec19db771af58c6c95c3dae1cd
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-01-28 18:16:25 -0600

    Copying over HAML's testing framework, despite being broken

--- test/lib/generators/slim/scaffold_generator_test.rb
+++ test/lib/generators/slim/scaffold_generator_test.rb
@@ -12,15 +12,20 @@ class Slim::Generators::ScaffoldGeneratorTest < Rails::Generators::TestCase
   test "should invoke template engine" do
     run_generator
 
-    %w(index edit new show _form).each { |view| assert_file "app/views/product_lines/#{view}.html.slim" }
-    assert_no_file "app/views/layouts/product_lines.html.slim"
+    %w(index edit new show _form).each { |view| assert_file File.join "app", "views", "product_lines", "#{view}.html.slim" }
+    assert_no_file File.join "app", "views", "layouts", "product_lines.html.slim"
   end
 
   test "should revoke template engine" do
     run_generator
     run_generator ["product_line"], :behavior => :revoke
 
-    assert_no_file "app/views/product_lines"
-    assert_no_file "app/views/layouts/product_lines.html.slim"
+    assert_no_file File.join "app", "views", "product_lines"
+    assert_no_file File.join "app", "views", "layouts", "product_lines.html.slim"
+  end
+
+  test "should invoke form builder" do
+    run_generator %w(product_line title:string price:integer --template-engine slim --form-builder some-form-builder)
+    assert_no_file File.join "app", "views", "product_lines", "_form.html.slim"
   end
 end
