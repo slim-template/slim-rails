173a548ef97b1fa3ceaeab748d099ff72d613664
Gonzalo Rodríguez-Baltanás Díaz <siotopo@gmail.com>
2012-01-30 17:55:34 +0100

    Use the right generator

--- lib/slim-rails.rb
+++ lib/slim-rails.rb
@@ -3,11 +3,11 @@ require 'slim'
 
 module Slim
   module Rails
-    class Railtie < ::Rails::Railtie
-      unless ::Rails.version =~ /$3.1/
-        config.generators.template_engine :slim
+    class Railtie < ::Rails::Railtie              
+      if ::Rails.version.to_f >= 3.1
+        config.app_generators.template_engine :slim        
       else
-        config.app_generators.template_engine :slim
+        config.generators.template_engine :slim
       end
     end
   end
