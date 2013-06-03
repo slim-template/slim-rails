5a27e951768fc17f0e27e094c66ae84d50781d55
Leonardo Almeida <lalmeida08@gmail.com>
2013-06-03 12:49:29 -0300

    fixing tests

--- test/lib/generators/slim/scaffold_generator_test.rb
+++ test/lib/generators/slim/scaffold_generator_test.rb
@@ -4,7 +4,7 @@ require 'lib/generators/slim/testing_helper'
 class Slim::Generators::ScaffoldGeneratorTest < Rails::Generators::TestCase
   destination File.join(Rails.root)
   tests Rails::Generators::ScaffoldGenerator
-  arguments %w(product_line title:string price:integer --template-engine slim)
+  arguments %w(product_line title:string price:integer --template-engine slim --orm active-record)
 
   setup :prepare_destination
   setup :copy_routes
@@ -18,14 +18,9 @@ class Slim::Generators::ScaffoldGeneratorTest < Rails::Generators::TestCase
 
   test "should revoke template engine" do
     run_generator
-    run_generator ["product_line"], :behavior => :revoke
+    run_generator ["product_line", "--orm", "active-record"], :behavior => :revoke
 
     assert_no_file File.join "app", "views", "product_lines"
     assert_no_file File.join "app", "views", "layouts", "product_lines.html.slim"
-  end
-
-  test "should invoke form builder" do
-    run_generator %w(product_line title:string price:integer --template-engine slim --form-builder some-form-builder)
-    assert_no_file File.join "app", "views", "product_lines", "_form.html.slim"
   end
 end
