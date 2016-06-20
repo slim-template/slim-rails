43bb1d1088b64c612e53b894fa3b471da2f52997
Seiei Miyagi <hanachin@gmail.com>
2016-06-20 21:30:20 +0900

    extract regiser_engine3/register_engine4

--- lib/slim-rails/register_engine.rb
+++ lib/slim-rails/register_engine.rb
@@ -3,14 +3,24 @@ module Slim
     module RegisterEngine
       def self.register_engine(app, config)
         if ::Rails::VERSION::MAJOR == 3
-          if app.assets && app.assets.respond_to?(:register_engine)
-            app.assets.register_engine('.slim', Slim::Template)
-          end
+          register_engine3(app, config)
         else
-          config.assets.configure do |env|
-            env.register_engine('.slim', Slim::Template)
-          end if config.respond_to?(:assets)
+          register_engine4(app, config)
         end
+      end
+
+      private
+
+      def self.register_engine3(app, _config)
+        if app.assets && app.assets.respond_to?(:register_engine)
+          app.assets.register_engine('.slim', Slim::Template)
+        end
+      end
+
+      def self.register_engine4(_app, config)
+        config.assets.configure do |env|
+          env.register_engine('.slim', Slim::Template)
+        end if config.respond_to?(:assets)
       end
     end
   end
