3ba52d9809cdac0a1bc651f213fa67cd470ab972
Seiei Miyagi <hanachin@gmail.com>
2016-06-20 21:36:54 +0900

    Rename register_engine3/register_engin4 to _register_engine3/_register_engine and drop unnecessary args.

--- lib/slim-rails/register_engine.rb
+++ lib/slim-rails/register_engine.rb
@@ -3,22 +3,22 @@ module Slim
     module RegisterEngine
       def self.register_engine(app, config)
         if ::Rails::VERSION::MAJOR == 3
-          register_engine3(app, config)
+          _register_engine3(app)
         else
-          register_engine4(app, config)
+          _register_engine(config)
         end
       end
 
       private
 
-      def self.register_engine3(app, _config)
+      def self._register_engine3(app)
         return unless app.assets
         return unless app.assets.respond_to?(:register_engine)
 
         app.assets.register_engine('.slim', Slim::Template)
       end
 
-      def self.register_engine4(_app, config)
+      def self._register_engine(config)
         return unless config.respond_to?(:assets)
 
         config.assets.configure do |env|
