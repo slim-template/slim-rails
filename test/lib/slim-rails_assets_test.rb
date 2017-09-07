785432888ae637f0a5e5b115606f5cef264bb257
kmossdorf <k.mossdorf@mediaeventservices.com>
2017-09-07 16:15:18 +0200

    Add ability to use slim in conjunction with ejs and jst

--- test/lib/slim-rails_assets_test.rb
+++ test/lib/slim-rails_assets_test.rb
@@ -18,9 +18,23 @@ class Slim::Rails::AssetsTest < ActiveSupport::TestCase
       #{code}
       APP
 
+      # Test HTML creation
       asset_path = File.join(dir, 'app', 'assets', 'html', 'test.slim')
       FileUtils.mkdir_p(File.dirname(asset_path))
-      File.write(asset_path, ".test\n  | hi")
+      File.write(asset_path, ".test\n | hi")
+
+      asset_path = File.join(dir, 'app', 'assets', 'html', 'test.js.slim')
+      FileUtils.mkdir_p(File.dirname(asset_path))
+      content = <<-EOH
+        ruby:
+          isTrue = true
+
+        .test
+          - if true
+            javascript:
+              var x = 42;
+      EOH
+      File.write(asset_path, content)
 
       `BUNDLE_GEMFILE=#{ENV['BUNDLE_GEMFILE']} bundle exec ruby #{app_path}`
     end
@@ -30,5 +44,11 @@ class Slim::Rails::AssetsTest < ActiveSupport::TestCase
     assert_equal 'ok', with_app(false, 'print DummyApp.assets || "ok"')
     assert_equal '<div class="test">hi</div>', with_app(true, 'print DummyApp.assets["test.slim"].to_s')
     assert_equal '<div class="test">hi</div>', with_app(true, 'print DummyApp.assets["test", accept: "text/html"].to_s')
+  end
+
+  test 'compile javascript slim view' do
+    assert_equal 'ok', with_app(false, 'print DummyApp.assets || "ok"')
+    assert_equal '<div class="test"><script>var x = 42;</script></div>', with_app(true, 'print DummyApp.assets["test.js.slim"].to_s.strip')
+    assert_equal '<div class="test"><script>var x = 42;</script></div>', with_app(true, 'print DummyApp.assets["test.js.slim", accept: "text/html"].to_s')
   end
 end
