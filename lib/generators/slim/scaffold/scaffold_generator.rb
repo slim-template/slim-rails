52c26c4b352f5bb22f270c59991f48d8e407f2ac
David Wessman <david.wesmn@gmail.com>
2022-05-03 07:11:54 +0200

    Restore templates for Rails < 7

--- lib/generators/slim/scaffold/scaffold_generator.rb
+++ lib/generators/slim/scaffold/scaffold_generator.rb
@@ -3,7 +3,11 @@ require "rails/generators/erb/scaffold/scaffold_generator"
 module Slim
   module Generators
     class ScaffoldGenerator < Erb::Generators::ScaffoldGenerator # :nodoc:
-      source_root File.expand_path(File.join("..", "templates"), __FILE__)
+      if ::Rails::VERSION::MAJOR >= 7
+        source_root File.expand_path(File.join("..", "templates"), __FILE__)
+      else
+        source_root File.expand_path(File.join("..", "legacy_templates"), __FILE__)
+      end
 
       def copy_view_files
         available_views.each do |view|
@@ -11,7 +15,9 @@ module Slim
           template "#{view}.html.slim", File.join("app", "views", controller_file_path, filename)
         end
 
-        template "partial.html.slim", File.join("app/views", controller_file_path, "_#{singular_name}.html.slim")
+        if ::Rails::VERSION::MAJOR >= 7
+          template "partial.html.slim", File.join("app/views", controller_file_path, "_#{singular_name}.html.slim")
+        end
       end
 
       hook_for :form_builder, as: :scaffold
