11166e521ab076aa9ca3d30cc6de352d2d543d56
Janusz Mordarski <janusz.m@gmail.com>
2022-05-10 19:08:33 +0200

    Use explicit begin block for Ruby < 2.5 and ignore it by Rubocop

--- lib/slim-rails.rb
+++ lib/slim-rails.rb
@@ -13,21 +13,23 @@ module Slim
 
         ActiveSupport.on_load(:action_view) do
           ActiveSupport.on_load(:after_initialize) do
-            if defined?(CacheDigests::DependencyTracker)
-              # 'cache_digests' gem being used (overrides Rails 4 implementation)
-              CacheDigests::DependencyTracker.register_tracker :slim, CacheDigests::DependencyTracker::ERBTracker
+            begin # rubocop:disable Style/RedundantBegin
+              if defined?(CacheDigests::DependencyTracker)
+                # 'cache_digests' gem being used (overrides Rails 4 implementation)
+                CacheDigests::DependencyTracker.register_tracker :slim, CacheDigests::DependencyTracker::ERBTracker
 
-              if ::Rails.env.development?
-                # recalculate cache digest keys for each request
-                CacheDigests::TemplateDigestor.cache = ActiveSupport::Cache::NullStore.new
+                if ::Rails.env.development?
+                  # recalculate cache digest keys for each request
+                  CacheDigests::TemplateDigestor.cache = ActiveSupport::Cache::NullStore.new
+                end
+              elsif ::Rails.version.to_s >= "4.0"
+                # will only apply if Rails 4, which includes 'action_view/dependency_tracker'
+                require "action_view/dependency_tracker"
+                ActionView::DependencyTracker.register_tracker :slim, ActionView::DependencyTracker::ERBTracker
               end
-            elsif ::Rails.version.to_s >= "4.0"
-              # will only apply if Rails 4, which includes 'action_view/dependency_tracker'
-              require "action_view/dependency_tracker"
-              ActionView::DependencyTracker.register_tracker :slim, ActionView::DependencyTracker::ERBTracker
+            rescue
+              # likely this version of Rails doesn't support dependency tracking
             end
-          rescue
-            # likely this version of Rails doesn't support dependency tracking
           end
         end
       end
